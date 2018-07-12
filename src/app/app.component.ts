7d282ee00e783bdb96626155da802248468ee61e
Marcelo Lúcio <marcelo.lucio1982@gmail.com>
2018-07-12 14:36:31 -0300

    variaveis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Http } from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  retorno: any = {};
+  variaveis = environment;
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.http.get('./variaveis.json')
+      .subscribe(data => {
+
+        this.retorno = data.json();
+
+        environment.production = this.retorno.producao;
+        environment.backend = this.retorno.backend;
+
+      }, error => console.log(error));
+  }
+
 }
